466d428c30aca00a9edee25ddad5d34628e76043
haiko <hvdschaaf@gmail.com>
2018-06-11 11:29:57 +0200

    correct counting non characters

--- src/main/java/nl/ritense/javapuzzles/DefaultNumberOfWordsFinder.java
+++ src/main/java/nl/ritense/javapuzzles/DefaultNumberOfWordsFinder.java
@@ -1,5 +1,7 @@
 package nl.ritense.javapuzzles;
 
+import org.springframework.util.StringUtils;
+
 import java.util.*;
 
 public class DefaultNumberOfWordsFinder implements NumberOfWordsFinder {
@@ -9,6 +11,29 @@ public class DefaultNumberOfWordsFinder implements NumberOfWordsFinder {
 
         //TODO implement solution here
 
-        return new ArrayList<>();
+        Map<String, Integer> wordMap = new HashMap<>();
+
+        String[] words = txt.split("\\s");
+
+        for(int i=0; i < words.length; i++){
+            String word = words[i];
+            if(StringUtils.hasText(word)){
+                Integer countWord = 1;
+                if(wordMap.containsKey(word)){
+                    countWord = wordMap.get(word) + 1;
+                }
+                wordMap.put(word, countWord);
+            }
+        }
+
+        List<WordNumber> wordNumberList = new ArrayList<>();
+
+        Set<String> wordSet = wordMap.keySet();
+
+        for (String word: wordSet) {
+            wordNumberList.add(new WordNumber(word, wordMap.get(word)));
+        }
+        return wordNumberList;
+        //return new ArrayList<>();
     }
 }
