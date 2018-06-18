6221a4c3749ab7b696029c598628af212654a5af
haiko <hvdschaaf@gmail.com>
2018-06-18 09:05:20 +0200

    change test

--- src/test/java/nl/ritense/javapuzzles/DuplicateFinderTest.java
+++ src/test/java/nl/ritense/javapuzzles/DuplicateFinderTest.java
@@ -13,19 +13,22 @@ public class DuplicateFinderTest {
 
     @Test
     public void shouldFindDuplicates() {
-        int[] arrayWithDuplicates = {1, 1, 3, 5, 5, 8, 8, 12, 1, 3, 14, 30, 4, 32, 4, 1, 7, 8, 0, 2, 6, 7, 20, 20, 21, 21, 23, 4, 6, 7, 8};
+        int[] arrayWithDuplicates = {1, 1, 3, 5, 5, 8, 8, 12, 1, 3, 14, 30, 4, 32, 4, 12, 1, 7, 8, 0, 2, 6, 7, 20, 20, 21, 21, 23, 4, 6, 7, 8};
 
         DuplicateFinder duplicateFinder = new DefaultDuplicateFinder();
-        Integer[] unduppedArray = duplicateFinder.findDuplicates(arrayWithDuplicates);
+        Integer[] dups = duplicateFinder.findDuplicates(arrayWithDuplicates);
 
 
-        assertEquals("length", 16, unduppedArray.length);
-        assertThat(unduppedArray, hasItemInArray(1));
-        assertThat(unduppedArray, hasItemInArray(3));
-        assertThat(unduppedArray, hasItemInArray(5));
-        assertThat(unduppedArray, hasItemInArray(8));
-        assertThat(unduppedArray, hasItemInArray(12));
-        assertThat(unduppedArray, hasItemInArray(20));
+        assertEquals("length", 16, dups.length);
+        assertThat(dups, hasItemInArray(1));
+        assertThat(dups, hasItemInArray(3));
+        assertThat(dups, hasItemInArray(5));
+        assertThat(dups, hasItemInArray(8));
+        assertThat(dups, hasItemInArray(12));
+        assertThat(dups, hasItemInArray(20));
+
+        assertTrue(dups[0].equals(Integer.valueOf(1)));
+        assertTrue(dups[15].equals(Integer.valueOf(8)));
 
     }
 }
