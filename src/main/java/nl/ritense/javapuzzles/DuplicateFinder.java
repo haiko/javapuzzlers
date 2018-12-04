bc95c5c566ba417f827d87b0cd67d6314eb34bfc
Haiko <hvdschaaf@gmail.com>
2018-12-04 09:25:09 +0100

    Update DuplicateFinder.java
    add documentation for clarification

--- src/main/java/nl/ritense/javapuzzles/DuplicateFinder.java
+++ src/main/java/nl/ritense/javapuzzles/DuplicateFinder.java
@@ -5,7 +5,11 @@ public interface DuplicateFinder {
 
     /**
      * Finds duplicates in array and returns duplicates in array in order found.
+     * The returning array must not be deduplicated itself.
      *
+     * So for an array with 1,3,3,5,8,9,1,7,8,9,8
+     *
+     * the returning array would be 3,1,8,9,8
      * @param duplicatesArray
      * @return
      */
